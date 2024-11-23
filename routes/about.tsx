7ee7b0af89f74c5c01290e0bc29f617fee063726
Jagger <jiangyujian@duck.com>
2024-11-23 18:40:41 +0800

    delete

--- routes/about.tsx
+++ routes/about.tsx
@@ -1,11 +1,11 @@
-import BigNumber from "npm:bignumber.js";
+// import BigNumber from "npm:bignumber.js";
 export default function About() {
-    const number = new BigNumber(0.2).plus(0.1).toString();
+    // const number = new BigNumber(0.2).plus(0.1).toString();
 
     return (
         <main>
             <h1>About</h1>
-            <p>{number}</p>
+            {/* <p>{number}</p> */}
         </main>
     );
 }
